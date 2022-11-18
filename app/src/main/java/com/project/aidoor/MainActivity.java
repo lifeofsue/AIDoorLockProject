6a158104775ea5441c20481f175d63f85151f448
sumin <chocosumin@cu.ac.kr>
2022-11-18 16:33:01 +0900

    Add: (11/18) 앱 실행 첫 화면에 onboarding 연결 & 버튼 클릭시 sing in/up 페이지로 이동 & logup.xml과 login.xml 화면 비율 조정

--- app/src/main/java/com/project/aidoor/MainActivity.java
+++ app/src/main/java/com/project/aidoor/MainActivity.java
@@ -2,17 +2,40 @@ package com.project.aidoor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button mSignUpBtn, mSignInBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.onboarding);
 
+        mSignUpBtn = findViewById(R.id.Btn_Signup);
+        mSignInBtn = findViewById(R.id.Btn_Signin);
+
+        mSignUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mSignInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

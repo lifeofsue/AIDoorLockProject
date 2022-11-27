8708790a3f37f334508057e906acd08c91847158
Dahyeon <1s2s2s2@naver.com>
2022-11-27 20:21:01 +0900

    수정본입니다

--- app/src/main/java/com/project/aidoor/MainActivity.java
+++ app/src/main/java/com/project/aidoor/MainActivity.java
@@ -2,10 +2,14 @@ package com.project.aidoor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.graphics.Canvas;
 import android.os.Bundle;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +17,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.onboarding);
-
     }
 }

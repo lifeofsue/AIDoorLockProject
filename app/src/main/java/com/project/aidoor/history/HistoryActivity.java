7cb39617f5c8340283317457a4440d9df44e1644
Sue Yoon <syy111391@gmail.com>
2022-12-02 03:06:43 +0900

    WIP: connect firebase

--- app/src/main/java/com/project/aidoor/history/HistoryActivity.java
+++ app/src/main/java/com/project/aidoor/history/HistoryActivity.java
@@ -1,16 +1,25 @@
 package com.project.aidoor.history;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
 import com.project.aidoor.R;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 
 public class HistoryActivity extends AppCompatActivity implements View.OnClickListener, HistoryViewListener
@@ -18,6 +27,10 @@ public class HistoryActivity extends AppCompatActivity implements View.OnClickLi
 
     private ArrayList<HistoryItem> historyItems = null;
     private HistoryAdapter historyAdapter = null;
+//    private String username = null;
+
+    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    DatabaseReference databaseReference = firebaseDatabase.getReference();
 
 
     @Override
@@ -28,6 +41,14 @@ public class HistoryActivity extends AppCompatActivity implements View.OnClickLi
 
         init();
 //        initView();
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        addChildEvent();
     }
 
 
@@ -53,6 +74,8 @@ public class HistoryActivity extends AppCompatActivity implements View.OnClickLi
     private void init()
     {
         historyItems = new ArrayList<>();
+
+//        username = "user_"+new Random().nextInt(1000);
     }
 
 
@@ -67,5 +90,40 @@ public class HistoryActivity extends AppCompatActivity implements View.OnClickLi
 //        historyAdapter = new HistoryAdapter(historyItems, this, this);
 //        recyclerView.setLayoutManager(layoutManager);
 //        recyclerView.setAdapter(historyAdapter);
+    }
+
+    private void addChildEvent()
+    {
+        databaseReference.child("History").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Log.d("sueyeon","addChileEvent in");
+                HistoryItem item = dataSnapshot.getValue(HistoryItem.class);
+
+                historyItems.add(item);
+                historyAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
